4a566ac0d2921b15757e34c61910e6d28562e1ea
Io <wmt.tkn.121@gmail.com>
2022-07-27 23:04:02 +0900

    implemented step23: put it together in package

---
+++ mydezero/__init__.py
@@ -0,0 +1,9 @@
+from mydezero.core_simple import Variable
+from mydezero.core_simple import Function
+from mydezero.core_simple import using_config
+from mydezero.core_simple import no_grad
+from mydezero.core_simple import as_array
+from mydezero.core_simple import as_variable
+from mydezero.core_simple import setup_variable
+
+setup_variable()
